be97e3ea01d1d88c7b9e3245def8ab0c56d6d6f6
tristripe <lrolfg@gmail.com>
2022-06-21 15:23:36 +0300

    links change

--- src/pages/indexPage/indexPage.ts
+++ src/pages/indexPage/indexPage.ts
@@ -10,8 +10,8 @@ export class IndexPage extends Block {
                 <p><a href="/login.html">Логин</a></p>
                 <p><a href="/register.html">Регистрация</a></p>
                 <p><a href="/settings.html">Настройки</a></p>
-                <p><a href="/friends.html">Список чатов (заглушка)</a></p>
-                <p><a href="/chat.html">Чат (заглушка)</a></p>
+                <p><a href="/chat.html">Список чатов</a></p>
+                <p><a href="/friends.html">Чат</a></p>
                 <p><a href="/404.html">404</a></p>
                 <p><a href="/500.html">500</a></p>
             </div>
